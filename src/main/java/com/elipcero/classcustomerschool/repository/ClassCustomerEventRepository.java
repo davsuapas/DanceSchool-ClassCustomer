b29473d8656cefb386341df76655eb96109b3066
dav.sua.pas@gmail.com <dav.sua.pas@gmail.com>
2018-08-09 12:20:57 +0200

    forked from DanceSchool

---
+++ src/main/java/com/elipcero/classcustomerschool/repository/ClassCustomerEventRepository.java
@@ -0,0 +1,7 @@
+package com.elipcero.classcustomerschool.repository;
+
+import com.elipcero.classcustomerschool.domain.ClassCustomerEvent;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ClassCustomerEventRepository extends CrudRepository<ClassCustomerEvent, Long> {
+}
